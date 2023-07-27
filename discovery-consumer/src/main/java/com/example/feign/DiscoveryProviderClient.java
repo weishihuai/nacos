720dfcb94b1cc38d04cd827b1e3391a2f3f4c290
weishihuai <775334252@qq.com>
2023-07-27 11:43:37 +0800

    openfeign示例

--- discovery-consumer/src/main/java/com/example/feign/DiscoveryProviderClient.java
+++ discovery-consumer/src/main/java/com/example/feign/DiscoveryProviderClient.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(value = "discovery-provider", contextId = "DiscoveryProviderClient")
-@RequestMapping("/provider")
 public interface DiscoveryProviderClient {
 
-    @RequestMapping(value = "/feign/{text}", method = RequestMethod.GET)
+    @RequestMapping(value = "/provider/feign/{text}", method = RequestMethod.GET)
     String feign(@PathVariable(value = "text") String text);
 }
